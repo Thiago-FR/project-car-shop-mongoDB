913fce61de413ca877b70efd84772ce051263a4a
Thiago <thiago_ssfr@hotmail.com>
2022-05-25 12:46:42 -0300

    feat: create services

---
+++ src/services/index.ts
@@ -0,0 +1,24 @@
+import { ZodError } from 'zod';
+import { Model } from '../interfaces/ModelInterface';
+
+export interface ServiceError {
+  error: ZodError;
+}
+
+abstract class Service<T> {
+  constructor(protected model: Model<T>) {}
+
+  async create(obj: T): Promise<T | null | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  async read(): Promise<T[]> {
+    return this.model.read();
+  }
+
+  async readOne(id: string): Promise<T | null | ServiceError> {
+    return this.model.readOne(id);
+  }
+}
+
+export default Service;
