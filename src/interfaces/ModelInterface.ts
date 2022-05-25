e55b6cfc44a40d76d1fccbc8ec86c41dda73737b
Thiago <thiago_ssfr@hotmail.com>
2022-05-25 10:32:09 -0300

    feat: create interface Model

---
+++ src/interfaces/ModelInterface.ts
@@ -0,0 +1,10 @@
+interface Model<T> {
+  create(obj: T): Promise<T>,
+  read(): Promise<T[]>,
+  readOne(id_: string): Promise<T | null>,
+  update(id_: string, obj: T): Promise<T | null>,
+  delete(id_: string): Promise<T | null>,
+}
+
+export default Model;
+export { Model };
